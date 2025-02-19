e6e591aaa80d2634956ac0bd5832432abb4f6d37
dlee9240 <dlee9240@gmail.com>
2025-02-19 14:50:22 -0800

    Updated main and ignore file

--- main.py
+++ main.py
@@ -2,9 +2,34 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(F"Screen height: {SCREEN_HEIGHT}")
+
+    #initialize pygame
+    pygame.init()
+
+    #set window size
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+
+
+
+        
+
+
+
 
 
 if __name__ == "__main__":
