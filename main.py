49f0f689a7c8d601429c9f334565d41283658225
dlee9240 <dlee9240@gmail.com>
2025-02-24 14:57:48 -0800

    Finish Moving Around Module

--- main.py
+++ main.py
@@ -28,16 +28,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        #Solution had this as the top line in the while statement...
+        player_1.update(dt)    
         screen.fill("black")
-        player_1.draw(screen)
+        player_1.draw(screen) 
         pygame.display.flip()
        
-
         #FPS
         dt = clock.tick(60) / 1000
-
-    
-
 
 
 if __name__ == "__main__":
