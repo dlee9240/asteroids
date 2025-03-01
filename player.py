ac611c331a245d32a21f449dba7362817f9e2b08
dlee9240 <dlee9240@gmail.com>
2025-03-01 15:17:31 -0800

    Module 4 Asteroids - Shot Timer

--- player.py
+++ player.py
@@ -10,6 +10,7 @@ class Player(CircleShape):
         super().__init__(x,y,PLAYER_RADIUS)
 
         self.rotation = 0
+        self.timer = 0
     
 
     # in the player class
@@ -30,6 +31,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer -= dt
         
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -42,12 +44,17 @@ class Player(CircleShape):
         if keys[pygame.K_SPACE]:
             self.shoot()
 
+            
+
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
         #This method is a problem....
+        if self.timer > 0:
+            return
+        self.timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
         
