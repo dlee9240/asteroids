aa7391bc1086241ea2cb716f82d8771379dd3521
dlee9240 <dlee9240@gmail.com>
2026-03-07 17:31:11 -0800

    Added clock/tick

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     #create inifinite while loop
     while True:
@@ -16,6 +18,9 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
+
     
 
 if __name__ == "__main__":
