18ef0eb9b2e8b4dd99ba8bc35cef3b15b38e58f1
Daniel Lee <dlee9240@gmail.com>
2026-03-10 20:45:36 -0700

    Chapter 4-1

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -11,10 +13,14 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
 
     #GAME LOOP
     while True:
