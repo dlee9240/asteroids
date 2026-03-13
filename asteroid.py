bfc9d9904b0f9a9b6c9c22e3e3dba952266352a7
dlee9240 <dlee9240@gmail.com>
2026-03-12 19:17:27 -0700

    Finished lesson

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,8 @@
 from constants import *
 from circleshape import CircleShape
 import pygame
-
+from logger import log_event
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -13,6 +14,40 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def asteroid_split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            random_angle = random.uniform(20,50)
+            new_rotation1 = self.velocity.rotate(random_angle)
+            new_rotation2 = self.velocity.rotate(-random_angle)
+            new_x = self.position.x
+            new_y = self.position.y
+            #new_asteroid = Asteroid(self.x, self.y, self.radius)
+            #new_asteroid.velocity.rotate(-random_angle)
+
+            #set the new radius for both asteroids
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            
+
+            child_asteroid1 = Asteroid(new_x, new_y, new_radius)
+            child_asteroid2 = Asteroid(new_x, new_y, new_radius)
+            
+            #self.velocity = pygame.Vector2(0, 0)
+            child_asteroid1.velocity = new_rotation1 * 1.2
+            child_asteroid2.velocity = new_rotation2 * 1.2
+
+
+
+
+
+
+
+
+
 
 
     
--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def main():
                 if s.collides_with(a):
                     log_event("asteroid_shot")
                     s.kill()
-                    a.kill()
+                    a.asteroid_split()
                 
 
         
